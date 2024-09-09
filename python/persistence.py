4bba3b03d0f1dea4fd0a764a0e074f983f862274
Álvaro <alozano@unizar.es>
2024-09-09 11:12:06 +0200

    import future should be the first statement

--- python/persistence.py
+++ python/persistence.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import sys
 from MorseAnalysis import VolumeImage, VectorField
-from __future__ import print_function
 
 infinity = float('inf')
 
